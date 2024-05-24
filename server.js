d43c4dd599f8a472fa154effcd3cc8bda823eba8
ZakhaRDevel <zakhar.devel@gmail.com>
2024-05-24 16:34:13 +0300

    feat(app): refactor delete method

--- server.js
+++ server.js
@@ -8,107 +8,135 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/users', function (req, res) {
-    const {name, email, phone, sortBy, order} = req.query;
+  const { name, email, phone, sortBy, order } = req.query;
 
-    fs.readFile('db.json', 'utf-8', function (err, data) {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Ошибка чтения файла');
-            return;
-        }
+  fs.readFile('db.json', 'utf-8', function (err, data) {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Ошибка чтения файла');
+      return;
+    }
 
-        let users = JSON.parse(data).users;
+    let users = JSON.parse(data).users;
 
-        if (name) {
-            users = users.filter(user => user.name.toLowerCase().includes(name.toLowerCase()));
-        }
-        if (email) {
-            users = users.filter(user => user.email.toLowerCase().includes(email.toLowerCase()));
-        }
-        if (phone) {
-            users = users.filter(user => user.phone.includes(phone));
-        }
+    if (name) {
+      users = users.filter((user) =>
+        user.name.toLowerCase().includes(name.toLowerCase())
+      );
+    }
+    if (email) {
+      users = users.filter((user) =>
+        user.email.toLowerCase().includes(email.toLowerCase())
+      );
+    }
+    if (phone) {
+      users = users.filter((user) => user.phone.includes(phone));
+    }
 
-        if (sortBy) {
-            users.sort((a, b) => {
-                if (a[sortBy] < b[sortBy]) {
-                    return order === 'desc' ? 1 : -1;
-                }
-                if (a[sortBy] > b[sortBy]) {
-                    return order === 'desc' ? -1 : 1;
-                }
-                return 0;
-            });
+    if (sortBy) {
+      users.sort((a, b) => {
+        if (a[sortBy] < b[sortBy]) {
+          return order === 'desc' ? 1 : -1;
         }
+        if (a[sortBy] > b[sortBy]) {
+          return order === 'desc' ? -1 : 1;
+        }
+        return 0;
+      });
+    }
 
-        res.json(users);
-    });
+    res.json(users);
+  });
 });
 
 app.put('/users', function (req, res) {
-    const newTaskList = req.body;
-    const jsonContent = JSON.stringify({ users: newTaskList }, null, 2);
+  const newTaskList = req.body;
+  const jsonContent = JSON.stringify({ users: newTaskList }, null, 2);
 
-    fs.writeFile('db.json', jsonContent, 'utf8', function (err) {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Ошибка записи в файл');
-            return;
-        }
-        res.status(200).json({ message: 'Список задач успешно обновлен' });
-    });
+  fs.writeFile('db.json', jsonContent, 'utf8', function (err) {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Ошибка записи в файл');
+      return;
+    }
+    res.status(200).json({ message: 'Список задач успешно обновлен' });
+  });
 });
 
 app.post('/users', function (req, res) {
-    const newTask = req.body;
-    fs.readFile('db.json', 'utf8', function (err, data) {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Ошибка чтения файла');
-            return;
-        }
-        const taskList = JSON.parse(data);
-        newTask.id = taskList.users.length;
-        taskList.users.push(newTask);
-        const jsonContent = JSON.stringify(taskList, null, 2);
-        fs.writeFile('db.json', jsonContent, 'utf8', function (err) {
-            if (err) {
-                console.error(err);
-                res.status(500).send('Ошибка записи в файл');
-                return;
-            }
-            res.status(200).json({ message: 'Список успешно создан' });
-        });
+  const newTask = req.body;
+  fs.readFile('db.json', 'utf8', function (err, data) {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Ошибка чтения файла');
+      return;
+    }
+    const taskList = JSON.parse(data);
+    newTask.id = taskList.users.length;
+    taskList.users.push(newTask);
+    const jsonContent = JSON.stringify(taskList, null, 2);
+    fs.writeFile('db.json', jsonContent, 'utf8', function (err) {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Ошибка записи в файл');
+        return;
+      }
+      res.status(200).json({ message: 'Список успешно создан' });
     });
+  });
 });
 
 app.delete('/users', function (req, res) {
-    const userIds = req.body.ids; // Ожидаем массив идентификаторов в теле запроса
+  const userIds = req.body.ids;
 
-    if (!Array.isArray(userIds)) {
-        return res.status(400).json({message: 'Идентификаторы пользователей должны быть в виде массива'});
+  if (!Array.isArray(userIds)) {
+    return res
+      .status(400)
+      .json({
+        message: 'Идентификаторы пользователей должны быть в виде массива',
+      });
+  }
+
+  fs.readFile('db.json', 'utf8', function (err, data) {
+    if (err) {
+      console.error('Ошибка чтения файла:', err);
+      return res.status(500).send('Ошибка чтения файла');
     }
 
-    fs.readFile('db.json', 'utf8', function (err, data) {
-        if (err) {
-            console.error(err);
-            return res.status(500).send('Ошибка чтения файла');
+    try {
+      let taskLists = JSON.parse(data);
+
+      // Логирование для отладки
+      console.log('Идентификаторы для удаления:', userIds);
+      console.log('Список пользователей до удаления:', taskLists.users);
+
+      // Убедитесь, что тип данных user.id и элементов в userIds совпадают
+      taskLists.users = taskLists.users.filter(
+        (user) => !userIds.includes(user.id.toString())
+      );
+
+      console.log('Список пользователей после удаления:', taskLists.users);
+
+      fs.writeFile(
+        'db.json',
+        JSON.stringify(taskLists, null, 2),
+        'utf8',
+        function (err) {
+          if (err) {
+            console.error('Ошибка записи в файл:', err);
+            return res.status(500).send('Ошибка записи в файл');
+          }
+          res.status(200).json({ message: 'Пользователи успешно удалены' });
         }
-
-        let taskLists = JSON.parse(data);
-        taskLists.users = taskLists.users.filter(user => !userIds.includes(user.id));
-
-        fs.writeFile('db.json', JSON.stringify(taskLists, null, 2), 'utf8', function (err) {
-            if (err) {
-                console.error(err);
-                return res.status(500).send('Ошибка записи в файл');
-            }
-            res.status(200).json({message: 'Пользователи успешно удалены'});
-        });
-    });
+      );
+    } catch (parseErr) {
+      console.error('Ошибка парсинга JSON:', parseErr);
+      res.status(500).send('Ошибка обработки данных');
+    }
+  });
 });
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
